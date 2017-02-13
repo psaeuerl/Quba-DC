985b2d806a59463ea61691029fd29cd11b8cf231
PSaeuerl <p.saeuerl@gmail.com>
2017-02-13 13:53:04 +0100

    Updated CreateTableHandler

--- QubaDC/QubaDC/Separated/SMO/SeparatedCreateTableHandler.cs
+++ QubaDC/QubaDC/Separated/SMO/SeparatedCreateTableHandler.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using QubaDC.SMO;
 using QubaDC;
+using QubaDC.DatabaseObjects;
 
 namespace QubaDC.Separated.SMO
 {
@@ -30,25 +31,20 @@ namespace QubaDC.Separated.SMO
             {
                 //Create Table
                 String normalesCreateTable = SMORenderer.RenderCreateTable(createTable);
-                List<String> columns = new List<string>();
-                columns.AddRange(createTable.Columns);
-                columns.Add("startts");
-                columns.Add("endts");
-                columns.Add("guid");
 
-                List<String> columndefinitions = new List<string>();
-                columndefinitions.AddRange(createTable.ColumnDefinitions);
+                ////Create History Table
+                List<ColumnDefinition> columndefinitions = new List<ColumnDefinition>();
+                columndefinitions.AddRange(createTable.Columns);
                 columndefinitions.AddRange(SMORenderer.GetHistoryTableColumns());
-                //Create History Table
                 CreateTable ctHistTable = new CreateTable()
-                {
-                    ColumnDefinitions = columndefinitions.ToArray(),
-                    Columns = columns.ToArray(),
+                { 
+                    Columns = columndefinitions.ToArray(),
                     Schema = createTable.Schema,
                     TableName = createTable.TableName + "_hist"
                 };
                 String histCreateTable = SMORenderer.RenderCreateTable(ctHistTable,true);
                 //Query Schema, add tables to Schema
+                Schema x =  this.schemaManager.GetCurrentSchema();
                 //Commit everything
             });
         }
