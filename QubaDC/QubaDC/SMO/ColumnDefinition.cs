b90ca5c1492b4f90287792c4414d8876a169d22f
PSaeuerl <p.saeuerl@gmail.com>
2017-02-13 13:50:07 +0100

    added ColumnDefinitions

---
+++ QubaDC/QubaDC/SMO/ColumnDefinition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QubaDC.SMO
+{
+    public class ColumnDefinition
+    {
+        public String ColumName { get; set; }
+
+        public String DataType { get; set; }
+
+        public Boolean Nullable { get; set; }
+
+        public String AdditionalInformation { get; set; }
+    }
+}
