9f4719d11a33caddbb5bd1567c39224f9e3ad746
PSaeuerl <p.saeuerl@gmail.com>
2017-02-15 09:24:47 +0100

    Serialization

--- QubaDC/QubaDC.Tests/SchemaSerializationTests.cs
+++ QubaDC/QubaDC.Tests/SchemaSerializationTests.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using QubaDC.DatabaseObjects;
+using QubaDC.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace QubaDC.Tests
 {
     public class SchemaSerializationTests
     {
+        [Fact]
+        public void SerDeWorks()
+        {
+            Schema x = new Schema()
+            {
+                Tables = new Table[]
+                {
+
+                          new Table("schema","name","col1","col2")
+                }
+            };
+
+            String ser = JsonSerializer.SerializeObject(x);
+            Assert.NotNull(ser);
+            Schema y = JsonSerializer.DeserializeObject<Schema>(ser);
+            Assert.NotNull(y);
+            //TODO => equality check
+        }
     }
 }
---
+++ QubaDC/QubaDC/Utility/JsonSerializer.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QubaDC.Utility
+{
+    public class JsonSerializer
+    {
+
+        public static String SerializeObject(Object o)
+        {
+            string output = JsonConvert.SerializeObject(o);
+            return output;
+        }
+
+        public static T DeserializeObject<T>(String obj)
+        {
+            T result = JsonConvert.DeserializeObject<T>(obj);
+            return result;
+        }
+    }
+}
