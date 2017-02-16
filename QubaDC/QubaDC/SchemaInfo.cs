bda4d4bd942d2de5bad573de5ee6bcda480c6e71
PSaeuerl <p.saeuerl@gmail.com>
2017-02-16 09:13:34 +0100

    added schemainfo

---
+++ QubaDC/QubaDC/SchemaInfo.cs
@@ -0,0 +1,20 @@
+﻿using QubaDC.DatabaseObjects;
+using QubaDC.SMO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QubaDC
+{
+    public class SchemaInfo
+    {
+        public long? ID { get; set; }
+        public Schema Schema { get; set; }
+        public SchemaModificationOperator SMO { get; set; }
+
+        public DateTime TimeOfCreation { get; set; }
+
+    }
+}
