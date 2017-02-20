01194de6247af919d81888571eeb8cb1b5ad7b16
PSaeuerl <p.saeuerl@gmail.com>
2017-02-20 14:11:49 +0100

    small changes with table inheritance structure

--- QubaDC/QubaDC/CRUD/SelectTable.cs
+++ QubaDC/QubaDC/CRUD/SelectTable.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace QubaDC.CRUD
 {
-
-    public abstract class SelectTable
+    public abstract class SelectTable : Table
     {
         public String TableAlias { get; set; }
-        public String TableName { get; set; }
     }
 }
