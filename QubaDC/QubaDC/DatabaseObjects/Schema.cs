13e08d4f1da575248bca26798b230ff53059a149
PSaeuerl <p.saeuerl@gmail.com>
2017-02-20 14:12:17 +0100

    improved schema

--- QubaDC/QubaDC/DatabaseObjects/Schema.cs
+++ QubaDC/QubaDC/DatabaseObjects/Schema.cs
@@ -1,28 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using QubaDC.SMO;
+using QubaDC.CRUD;
 using QubaDC.Utility;
 
 namespace QubaDC.DatabaseObjects
 {
+
     public class Schema
     {
-        public IEnumerable<Table> Tables { get { return this._Tables; } }
-        public IEnumerable<Table> HistTables { get { return this._HistTables; } }
+        public IEnumerable<TableSchemaWithHistTable> Tables { get { return this._Tables; } }
+        public IEnumerable<TableSchema> HistTables { get { return this._HistTables; } }
 
-        private List<Table> _Tables { get ; set; } = new List<Table>();
+        private List<TableSchemaWithHistTable> _Tables { get ; set; } = new List<TableSchemaWithHistTable>();
 
-        private List<Table> _HistTables { get;  set; } = new List<Table>();      
+        private List<TableSchema> _HistTables { get;  set; } = new List<TableSchema>();      
 
-        public void AddTable(Table table, Table histequivalent)
+        public void AddTable(TableSchema table, TableSchema histequivalent)
         {
-            this._Tables.Add(table);
+            this._Tables.Add(new TableSchemaWithHistTable()
+            {
+                Table = table,
+                HistTableName = histequivalent.Name,
+                HistTableSchema = histequivalent.Schema
+            });
             this._HistTables.Add(histequivalent);
             //PS Not sure if i should check here also that the columns got more
             Guard.StateTrue(table.Name == histequivalent.Name + "_hist", "Table name does not equal histname_hist");
+        }
+
+        internal Table FindHistTable(Table insertTable)
+        {
+            TableSchemaWithHistTable withHist=
+                this._Tables.Find(x => x.Table.Name == insertTable.TableName && x.Table.Schema == insertTable.TableSchema);
+            TableSchema h = this._HistTables.Find(x => x.Schema == withHist.HistTableSchema && x.Name == withHist.HistTableName);
+            return h.ToTable();
         }
     }
 }
