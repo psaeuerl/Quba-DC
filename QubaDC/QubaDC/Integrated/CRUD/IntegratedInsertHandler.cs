a84b56ce5ce60309db33c50976db88d4f4c5c1bb
PSaeuerl <p.saeuerl@gmail.com>
2017-09-06 11:55:45 +0200

    inserthandler working

--- QubaDC/QubaDC/Integrated/CRUD/IntegratedInsertHandler.cs
+++ QubaDC/QubaDC/Integrated/CRUD/IntegratedInsertHandler.cs
@@ -31,13 +31,13 @@ namespace QubaDC.Integrated.CRUD
            insertOperation.ColumnNames = insertOperation.ColumnNames.Concat(new String[] { IntegratedConstants.StartTS, IntegratedConstants.EndTS }).ToArray();
            insertOperation.ValueLiterals = insertOperation.ValueLiterals.Concat(new String[]
            {
-                     MySQLDialectHelper.RenderDateTime(t),
+                    this.CRUDRenderer.renderDateTime(t),
                      "null"
            }).ToArray();
            String insertToTable = this.CRUDRenderer.RenderInsert(insertOperation.InsertTable, insertOperation.ColumnNames, insertOperation.ValueLiterals);
            String insertToGlobalUpdate = this.CRUDRenderer.RenderInsert(this.timeManager.GetTable(),
 new String[] { "Operation", "Timestamp" },
-new String[] { String.Format("'insert on {0}'", this.timeManager.GetTable().TableName), MySQLDialectHelper.RenderDateTime(t) }
+new String[] { String.Format("'insert on {0}'", this.timeManager.GetTable().TableName), this.CRUDRenderer.renderDateTime(t) }
 );
            return new String[]
            {
@@ -49,46 +49,11 @@ new String[] { String.Format("'insert on {0}'", this.timeManager.GetTable().Tabl
                 {
                    insertOperation.InsertTable.TableSchema+"."+insertOperation.InsertTable.TableName,
                    timeManager.GetTableName()
-                }; 
-            NewMethod(renderStaetement, lockTables);
-
+                };
+            IntegratedCRUDExecuter.ExecuteStatementsOnLockedTables(renderStaetement, lockTables, this.DataConnection, this.CRUDRenderer);
         }
 
-        private void NewMethod(Func<String[]> RenderStatements,String[] locktables)
-        {
-            this.DataConnection.AquiereOpenConnection(con =>
-            {
-                String[] lockTableStatements = this.CRUDRenderer.RenderLockTables(locktables);
-                try
-                {
-                    foreach (var setupSql in lockTableStatements)
-                        this.DataConnection.ExecuteNonQuerySQL(setupSql, con);
-                }
-                catch (Exception e)
-                {
-                    throw new InvalidOperationException("Could not aquire locks for:" + String.Join(",", locktables), e);
-                }
-
-            try
-            {
-                String[] statements = RenderStatements();
-                String[] success = this.CRUDRenderer.RenderCommitAndUnlock();
-                this.DataConnection.ExecuteNonQuerySQL(statements[0], con);
-                this.DataConnection.ExecuteNonQuerySQL(statements[1], con);
-                this.DataConnection.ExecuteNonQuerySQL(success[0], con);
-                this.DataConnection.ExecuteNonQuerySQL(success[1], con);
-
-                }
-                catch (Exception e)
-                {
-                    String[] rollbackAndUnlock = this.CRUDRenderer.RenderRollBackAndUnlock();
-                    this.DataConnection.ExecuteNonQuerySQL(rollbackAndUnlock[0]);
-                    this.DataConnection.ExecuteNonQuerySQL(rollbackAndUnlock[1]);
-                    throw new InvalidOperationException("Got exception after Table Locks, rolled back and unlocked", e);
-                }
-
-            });
-        }
+      
 
        
     }
