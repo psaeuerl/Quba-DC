7cfa2e43cb9b8b6ab7ce865e8df005a090bb17b9
PSaeuerl <p.saeuerl@gmail.com>
2017-09-10 01:14:09 +0200

    createtablehandler now working without locks

--- QubaDC/QubaDC/Integrated/SMO/IntegratedCreateTableHandler.cs
+++ QubaDC/QubaDC/Integrated/SMO/IntegratedCreateTableHandler.cs
@@ -14,57 +14,65 @@ namespace QubaDC.Integrated.SMO
     {
         private SchemaManager schemaManager;
 
-        public IntegratedCreateTableHandler(DataConnection c, SchemaManager schemaManager, SMORenderer renderer, GlobalUpdateTimeManager manager)
+        public IntegratedCreateTableHandler(DataConnection c, SchemaManager schemaManager,SMORenderer renderer)
         {
             this.DataConnection = c;
             this.schemaManager = schemaManager;
             this.SMORenderer = renderer;
-            this.GlobalUpdateTimemanager = manager;
         }
 
         public DataConnection DataConnection { get; private set; }
         public SMORenderer SMORenderer { get; private set; }
-        public GlobalUpdateTimeManager GlobalUpdateTimemanager { get; private set; }
 
         internal void Handle(CreateTable createTable)
         {
             var con = (MySQLDataConnection)DataConnection;
-            //Actual Code
-            ColumnDefinition[] histColumns = IntegratedConstants.GetHistoryTableColumns();
-
-            CreateTable newCt = JsonSerializer.CopyItem<CreateTable>(createTable);
-            newCt.Columns = createTable.Columns.Union(histColumns).ToArray();
-            String createBaseTable = SMORenderer.RenderCreateTable(newCt);
-
-            SchemaInfo xy = this.schemaManager.GetCurrentSchema();
-            Schema x = xy.Schema;
-            if (xy.ID == null)
+            con.DoTransaction((transaction, c) =>
             {
-                x = new Schema();
-                xy.ID = 0;
-            }
+                ////What to do?
+                ////Create Table normal
+                ////Create Table Hist
+                ////Create Trigger on normal
+                ColumnDefinition[] histColumns = IntegratedConstants.GetHistoryTableColumns();
 
-            ////Create History Table
-            CreateTable ctHistTable = CreateHistTable(createTable, xy);
-            String createHistTable = SMORenderer.RenderCreateTable(ctHistTable, true);
+                CreateTable newCt = JsonSerializer.CopyItem<CreateTable>(createTable);
+                newCt.Columns = createTable.Columns.Union(histColumns).ToArray();
+                String createBaseTable = SMORenderer.RenderCreateTable(newCt);
+                ////Create History Table
+                SchemaInfo xy = this.schemaManager.GetCurrentSchema(c);
+                Schema x = xy.Schema;
+                if (xy.ID == null)
+                {
+                    x = new Schema();
+                    xy.ID = 0;
+                }
 
-            String ensureSchemaVersion = schemaManager.RenderEnsureSchema(xy);
+                CreateTable ctHistTable = CreateHistTable(createTable, xy);
 
-            //Manage Schema Statement
-            x.AddTable(createTable.ToTableSchema(), ctHistTable.ToTableSchema());
+                String createHistTable = SMORenderer.RenderCreateTable(ctHistTable, true);
+
+                //Manage Schema Statement
+                x.AddTable(createTable.ToTableSchema(), ctHistTable.ToTableSchema());
+                //String updateSchema = this.schemaManager.GetInsertSchemaStatement(x, createTable);
+
+                //Add tables
+                con.ExecuteNonQuerySQL(createBaseTable, c);
+                con.ExecuteNonQuerySQL(createHistTable, c);
+
+                //////INsert Trigger 
+                //String trigger = SMORenderer.RenderCreateInsertTrigger(createTable.ToTableSchema(), ctHistTable.ToTableSchema());
+                //String updateTrigger = SMORenderer.RenderCreateUpdateTrigger(createTable.ToTableSchema(), ctHistTable.ToTableSchema());
+
+                //////Add Trigger
+                //con.ExecuteSQLScript(trigger, c);
+                //con.ExecuteSQLScript(updateTrigger, c);
 
 
-            String ensureSchema = schemaManager.RenderEnsureSchema(xy);
-            String insertNewSchema = this.schemaManager.GetInsertSchemaStatement(x, createTable);
 
-            String[] PreLockingStatements = new String[] { createBaseTable, createHistTable };
-            String[] AfterLockingStatemnts = new String[] { ensureSchema, insertNewSchema };
-            String[] tablesToLock = new String[] { this.schemaManager.GetTableName(), this.GlobalUpdateTimemanager.GetTableName() ,
-                                                             this.SMORenderer.CRUDRenderer.PrepareTable(createTable.ToTable()),
-                                                             this.SMORenderer.CRUDRenderer.PrepareTable(ctHistTable.ToTable()),
-                                                          };
-            IntegratedSMOExecuter.Execute(SMORenderer, con, PreLockingStatements, AfterLockingStatemnts, tablesToLock);
-
+                ////Store Schema
+                this.schemaManager.StoreSchema(x, createTable, con, c);
+                transaction.Commit();
+            });
         }
 
         private static CreateTable CreateHistTable(CreateTable createTable, SchemaInfo xy)
