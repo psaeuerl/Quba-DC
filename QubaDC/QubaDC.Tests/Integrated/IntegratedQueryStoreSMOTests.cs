e5e7a4b07672388371e8b3487dd1dfde1b6786c7
PSaeuerl <p.saeuerl@gmail.com>
2017-09-09 19:30:22 +0200

    base test setup

---
+++ QubaDC/QubaDC.Tests/Integrated/IntegratedQueryStoreSMOTests.cs
@@ -0,0 +1,41 @@
+﻿using QubaDC.CRUD;
+using QubaDC.Restrictions;
+using QubaDC.Separated;
+using QubaDC.SMO;
+using QubaDC.Tests.CustomAsserts;
+using QubaDC.Tests.DataBuilder;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace QubaDC.Tests.Separated
+{
+    public class IntegratedQueryStoreSMOTests :  SystemQueryStoreSMOTests
+    {
+
+        public IntegratedQueryStoreSMOTests() : base()
+        {
+            IntegratedQBDCFixture f = new IntegratedQBDCFixture();
+            this.SeparatedFixture = f;
+            this.Fixture = f;
+            base.Init();
+        }
+
+        public IntegratedQBDCFixture SeparatedFixture { get; private set; }
+
+        public override string BuildDataBaseName()
+        {
+            //Create Empty Schema
+            return "Integrated" + Guid.NewGuid().ToString().Replace("-", "");
+        }
+
+        public override QubaDCSystem BuildSystem()
+        {
+            return SeparatedFixture.QBDCSystem;
+        }
+    }
+}
