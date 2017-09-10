f9d27d59d47fe54664acb9085b3fe6bd8d7392c0
PSaeuerl <p.saeuerl@gmail.com>
2017-09-10 22:27:05 +0200

    renametablehandler working

--- QubaDC/QubaDC/Integrated/SMO/IntegratedRenameTableHandler.cs
+++ QubaDC/QubaDC/Integrated/SMO/IntegratedRenameTableHandler.cs
@@ -1,65 +1,66 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using QubaDC.SMO;
-//using QubaDC;
-//using QubaDC.DatabaseObjects;
-//using QubaDC.Utility;
-//using QubaDC.CRUD;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QubaDC.SMO;
+using QubaDC;
+using QubaDC.DatabaseObjects;
+using QubaDC.Utility;
+using QubaDC.CRUD;
 
-//namespace QubaDC.Separated.SMO
-//{
-//    public class IntegratedRenameTableHandler
-//    {
-//        private SchemaManager schemaManager;
+namespace QubaDC.Separated.SMO
+{
+    public class IntegratedRenameTableHandler
+    {
+        private SchemaManager schemaManager;
 
-//        public IntegratedRenameTableHandler(DataConnection c, SchemaManager schemaManager,SMORenderer renderer)
-//        {
-//            this.DataConnection = c;
-//            this.schemaManager = schemaManager;
-//            this.SMORenderer = renderer;
-//        }
+        public IntegratedRenameTableHandler(DataConnection c, SchemaManager schemaManager, SMORenderer renderer)
+        {
+            this.DataConnection = c;
+            this.schemaManager = schemaManager;
+            this.SMORenderer = renderer;
+        }
 
-//        public DataConnection DataConnection { get; private set; }
-//        public SMORenderer SMORenderer { get; private set; }
+        public DataConnection DataConnection { get; private set; }
+        public SMORenderer SMORenderer { get; private set; }
 
-//        internal void Handle(RenameTable renameTable)
-//        {
+        internal void Handle(RenameTable renameTable)
+        {
 
-//            //What to do here?
-//            //a.) Execute Rename Table
-//            //b.) Schemamanager => Add that new table points to old table
-//            //c.) Triggers can stay
+            //What to do here?
+            //a.) Execute Rename Table
+            //b.) Schemamanager => Add that new table points to old table
+            //c.) Triggers can stay
 
-//            var con = (MySQLDataConnection)DataConnection;
-//            con.DoTransaction((transaction, c) =>
-//            {
+            var con = (MySQLDataConnection)DataConnection;
+            con.DoTransaction((transaction, c) =>
+            {
 
-//                String renameTableSQL = SMORenderer.RenderRenameTable(renameTable);
 
-//                //Change Shchema    
-//                //take old table, remove it, add it with new names
-//                SchemaInfo xy = this.schemaManager.GetCurrentSchema();
-//                Schema x = xy.Schema;
-//                Table oldTable = new Table() { TableSchema = renameTable.OldSchema, TableName = renameTable.OldTableName };
-//                TableSchemaWithHistTable table = x.FindTable(oldTable);
-//                TableSchema tableHist = x.FindHistTable(oldTable);
-//                x.RemoveTable(oldTable);
-//                table.Table.Name = renameTable.NewTableName;
-//                table.Table.Schema = renameTable.NewSchema;
-//                x.AddTable(table.Table,tableHist);
 
-//                //Renameing Table
-//                con.ExecuteQuery(renameTableSQL, c);
+                String renameTableSQL = SMORenderer.RenderRenameTable(renameTable);
 
-//                this.schemaManager.StoreSchema(x, renameTable, con, c);
+                //Change Shchema    
+                //take old table, remove it, add it with new names
+                SchemaInfo xy = this.schemaManager.GetCurrentSchema();
+                Schema x = xy.Schema;
+                Table oldTable = new Table() { TableSchema = renameTable.OldSchema, TableName = renameTable.OldTableName };
+                TableSchemaWithHistTable table = x.FindTable(oldTable);
+                TableSchema tableHist = x.FindHistTable(oldTable);
+                var actualTable = x.FindTable(oldTable);
+                actualTable.Table.Name = renameTable.NewTableName;
+                actualTable.Table.Schema = renameTable.NewSchema;
 
-//                transaction.Commit();
+                //Renameing Table
+                con.ExecuteQuery(renameTableSQL, c);
 
-//            });
-//        }
+                this.schemaManager.StoreSchema(x, renameTable, con, c);
 
-//    }
-//}
+                transaction.Commit();
+
+            });
+        }
+
+    }
+}
