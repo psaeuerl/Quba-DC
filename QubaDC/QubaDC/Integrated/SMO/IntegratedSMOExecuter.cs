231b4aaba74dc52a9d62d5c2d302ff223416269c
PSaeuerl <p.saeuerl@gmail.com>
2017-09-18 20:07:50 +0200

    smoexecuter - locking implemented

--- QubaDC/QubaDC/Integrated/SMO/IntegratedSMOExecuter.cs
+++ QubaDC/QubaDC/Integrated/SMO/IntegratedSMOExecuter.cs
@@ -14,6 +14,10 @@ namespace QubaDC.Integrated.SMO
         public String[] UpdateStatements { get; set; }
         public Schema newSchema { get; set; }
 
+        public Table[] MetaTablesToLock { get; set; }
+
+        public Table[] TablesToUnlock { get; set; }
+
     }
     public class IntegratedSMOExecuter
     {
@@ -22,7 +26,6 @@ namespace QubaDC.Integrated.SMO
 
         public static void Execute(SMORenderer SMORenderer
             , DataConnection DataConnection
-            //, string[] PreLockingStatements
             //, string[] AfterLockingStatemnts
             //, string[] tablesToLock
             //, Boolean[] lockAsWrite
@@ -30,6 +33,7 @@ namespace QubaDC.Integrated.SMO
             , SchemaModificationOperator op
             , Func<SchemaInfo, UpdateSchema> RenderStatements
             , Action<String> logStatements
+            , TableMetadataManager metaManager
             )
         {
 
@@ -37,14 +41,24 @@ namespace QubaDC.Integrated.SMO
             String[] beforeLockStatements = new String[]
         {
                         "SET autocommit=0;",
-                        "SELECT GET_LOCK('SMO UPDATES',10);",
-                        "SET @updateTime = NOW(3); "
+                        "SELECT GET_LOCK('SMO UPDATES',10);"                       
         };
+
+            String[] afterTablesClearedStmt = new string[]
+            {
+                 "SET @updateTime = NOW(3); "
+            };
 
             String[] cleanup = new String[]
             {
                 "COMMIT;",
                 "SELECT RELEASE_LOCK('SMO UPDATES');"
+            };
+
+            String[] unlockCommit = new String[]
+            {
+                "COMMIT;",
+"UNLOCK TABLES; "
             };
             DataConnection.AquiereOpenConnection(con =>
             {
@@ -58,11 +72,46 @@ namespace QubaDC.Integrated.SMO
                     currentSchemaInfo.ID = 0;
                 }
 
-                UpdateSchema updateSchema = RenderStatements(currentSchemaInfo); 
+                UpdateSchema updateSchema = RenderStatements(currentSchemaInfo);
+
+                //Lock Tables
+                String[] tablesToLock = updateSchema.MetaTablesToLock.Select(x => metaManager.GetMetaTableFor(x.TableSchema, x.TableName))
+                                                                    .Select(x => SMORenderer.CRUDRenderer.PrepareTable(x))
+                                                                    .ToArray();
+                if (tablesToLock.Length > 0)
+                {
+                    Boolean[] lockAsWrite = tablesToLock.Select(x => true).ToArray();
+                    String[] lockTables = crudRenderer.RenderLockTables(tablesToLock, lockAsWrite);
+                    ExecuteStatements(DataConnection, con, lockTables, logStatements);
+
+                    //SetCanBeQueried
+                    String[] canBeQueriedFalse = updateSchema.MetaTablesToLock.Select(x => metaManager.SetCanBeQueriedFalse(x)).ToArray();
+                    ExecuteStatements(DataConnection, con, canBeQueriedFalse, logStatements);
+
+                    //Unlock tables
+                    ExecuteStatements(DataConnection, con, unlockCommit, logStatements);
+                } else if(typeof(CreateTable) != op.GetType())
+                {
+                    throw new InvalidOperationException("Only Create Table needs no tablesToLock");
+                }
+
+                ExecuteStatements(DataConnection, con, afterTablesClearedStmt, logStatements);
+
                 ExecuteStatements(DataConnection, con, updateSchema.UpdateStatements, logStatements);
-                String storeSchema =  schemaManager.GetInsertSchemaStatement(updateSchema.newSchema, op, true);
+                String storeSchema =  schemaManager.GetInsertSchemaStatement(updateSchema.newSchema, op, true);                
 
                 ExecuteStatements(DataConnection, con, new String[] { storeSchema }, logStatements);
+
+                //release Can Be Queried
+                String[] setCanBeQueriedTrue = updateSchema.TablesToUnlock.Select(x => metaManager.SetCanBeQueriedTrue(x)).ToArray();
+                if (setCanBeQueriedTrue.Length > 0)
+                {
+                    ExecuteStatements(DataConnection, con, setCanBeQueriedTrue, logStatements);
+                } else if(op.GetType() != typeof(CreateTable))
+                {
+                    throw new InvalidOperationException("Only create table needs no unlock");
+                }
+
 
                 ExecuteStatements(DataConnection, con, cleanup, logStatements);
             });
