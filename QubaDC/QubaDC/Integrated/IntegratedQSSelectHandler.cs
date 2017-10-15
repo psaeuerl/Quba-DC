bed2e7d4af5d5186ad6a8ec6337865af5b4ba8ca
PSaeuerl <p.saeuerl@gmail.com>
2017-10-15 03:11:47 +0200

    fixxed locking on integratedqsselect handler

--- QubaDC/QubaDC/Integrated/IntegratedQSSelectHandler.cs
+++ QubaDC/QubaDC/Integrated/IntegratedQSSelectHandler.cs
@@ -232,7 +232,7 @@ namespace QubaDC.Integrated
         //                Result = normaResult
         //            };
         //        }
-        public override QueryStoreSelectResult HandleSelect(SelectOperation s, SchemaManager schemaManager, DataConnection con, TableMetadataManager timeManager, CRUDVisitor cRUDHandler, QueryStore qs)
+        public override QueryStoreSelectResult HandleSelect(SelectOperation s, SchemaManager schemaManager, DataConnection con, TableMetadataManager metaManager, CRUDVisitor cRUDHandler, QueryStore qs)
         {
             //3.Open transaction and lock tables(I)
             //4.Execute(original) query and retrieve subset.
@@ -248,120 +248,149 @@ namespace QubaDC.Integrated
             //We query the global update timestampt and get the max from it
             //Then we get which schema was active there and get the history tables for this one            
 
+            TableToLock[] aliasTables = s.GetAllSelectedTables().Select(x => new TableToLock { Alias = x.TableAlias, LockAsWrite = false, Name = cRUDHandler.CRUDRenderer.PrepareTable(x) }).ToArray();
+            TableToLock[] metaTables = s.GetAllSelectedTables().Select(x => metaManager.GetMetaTableFor(x.TableSchema, x.TableName))
+                                                               .Select(x => new TableToLock { Alias = null, LockAsWrite = false, Name = cRUDHandler.CRUDRenderer.PrepareTable(x) })
+                                                               .ToArray();
 
-            //What to do here:
+            TableToLock[] tablesToLock = aliasTables.Union(metaTables).ToArray();
+
+            Table[] MetaTables = s.GetAllSelectedTables()//.Select(x => metaManager.GetMetaTableFor(x.TableSchema, x.TableName))
+                                .ToArray();
+
+           // String[] tablesToLock = s.GetAllSelectedTables().Select(x => new String[] { cRUDHandler.CRUDRenderer.PrepareTable(x));
+
             //0.) Get last updated Timestamp
-            var lastGlobalUpdate = timeManager.GetLatestUpdate(s.GetAllSelectedTables().Select(x=> new Table() { TableName = x.TableName, TableSchema = x.TableSchema }).ToArray());
-            var queryTime = lastGlobalUpdate;
 
-            //a.) copy the operation
-            var newOperation = JsonSerializer.CopyItem(s);
-            //b.) change all tables to the respective history ones + build restrictions for it
-            List<Restriction> TimeStampRestrictions = new List<Restriction>();
-            SchemaInfo SchemaInfo = schemaManager.GetSchemaActiveAt(queryTime);
-            Dictionary<String, Guid?> table_to_ids = new Dictionary<String, Guid?>();
+            Dictionary<String, Guid?> table_to_ids = null;
+            DateTime queryTime = DateTime.Now;
+            String originalrenderd = "";
+            String originalSerialized = "";
+            String RewrittenSerialized = "";
+            String select = "";
+            String selectHash = "";
+            String selectHashSerialized = "";
+            SelectOperation newOperation = null;
+            DataTable normResult = null;
+            Func<SchemaInfo, DateTime, String[]> RenderSelectStatements = (schema, dt) =>
+             {
+                 queryTime = dt;
 
-            foreach (var selectedTable in newOperation.GetAllSelectedTables())
-            {
-                var foundTabl = SchemaInfo.Schema.FindTable(selectedTable);
-                table_to_ids.Add(selectedTable.TableAlias, foundTabl.Table.AddTimeSetGuid);
-                //var histTable = SchemaInfo.Schema.FindHistTable(selectedTable).ToTable();
-                //selectedTable.TableName = histTable.TableName;
-                //selectedTable.TableSchema = histTable.TableSchema;
-                OperatorRestriction startTs = new OperatorRestriction()
-                {
-                    LHS = new ColumnOperand()
-                    {
-                        Column = new ColumnReference()
-                        {
-                            ColumnName = IntegratedConstants.StartTS,
-                            TableReference = selectedTable.TableAlias
-                        }
-                    },
-                    Op = RestrictionOperator.LET
-                    ,
-                    RHS = new DateTimeRestrictionOperand()
-                    {
-                        Value = queryTime
-                    },
-                };
+                 //a.) copy the operation
+                 newOperation = JsonSerializer.CopyItem(s);
+                 //b.) change all tables to the respective history ones + build restrictions for it
+                 List<Restriction> TimeStampRestrictions = new List<Restriction>();
+                 SchemaInfo SchemaInfo = schema;
+                 table_to_ids = new Dictionary<String, Guid?>();
 
-                OperatorRestriction endTsLt = new OperatorRestriction()
-                {
-                    LHS = new DateTimeRestrictionOperand()
-                    {
-                        Value = queryTime
-                    },
-                    Op = RestrictionOperator.LT
-    ,
-                    RHS = new ColumnOperand()
-                    {
-                        Column = new ColumnReference()
-                        {
-                            ColumnName = IntegratedConstants.EndTS,
-                            TableReference = selectedTable.TableAlias
-                        }
-                    },
-                };
-                OperatorRestriction endTSNull = new OperatorRestriction()
-                {
-                    LHS = new ColumnOperand()
-                    {
-                        Column = new ColumnReference()
-                        {
-                            ColumnName = IntegratedConstants.EndTS,
-                            TableReference = selectedTable.TableAlias
-                        }
-                    },
-                    Op = RestrictionOperator.IS
-,
-                    RHS = new LiteralOperand()
-                    {
-                        Literal = "NULL"
-                    }
-                };
-                var OrRestriction = new OrRestriction();
-                OrRestriction.Restrictions = new Restriction[] { endTsLt, endTSNull };
-                var AndRestriction = new AndRestriction();
-                AndRestriction.Restrictions = new Restriction[] { startTs, OrRestriction };
-                TimeStampRestrictions.Add(AndRestriction);
-            }
-            //c.) add timestamp parts to it
-            if (newOperation.Restriction != null)
-                TimeStampRestrictions.Add(newOperation.Restriction);
+                 foreach (var selectedTable in newOperation.GetAllSelectedTables())
+                 {
+                     var foundTabl = SchemaInfo.Schema.FindTable(selectedTable);
+                     table_to_ids.Add(selectedTable.TableAlias, foundTabl.Table.AddTimeSetGuid);
+                     //var histTable = SchemaInfo.Schema.FindHistTable(selectedTable).ToTable();
+                     //selectedTable.TableName = histTable.TableName;
+                     //selectedTable.TableSchema = histTable.TableSchema;
+                     OperatorRestriction startTs = new OperatorRestriction()
+                     {
+                         LHS = new ColumnOperand()
+                         {
+                             Column = new ColumnReference()
+                             {
+                                 ColumnName = IntegratedConstants.StartTS,
+                                 TableReference = selectedTable.TableAlias
+                             }
+                         },
+                         Op = RestrictionOperator.LET
+                          ,
+                         RHS = new DateTimeRestrictionOperand()
+                         {
+                             Value = queryTime
+                         },
+                     };
 
-            newOperation.Restriction = new AndRestriction() { Restrictions = TimeStampRestrictions.ToArray() };
+                     OperatorRestriction endTsLt = new OperatorRestriction()
+                     {
+                         LHS = new DateTimeRestrictionOperand()
+                         {
+                             Value = queryTime
+                         },
+                         Op = RestrictionOperator.LT
+         ,
+                         RHS = new ColumnOperand()
+                         {
+                             Column = new ColumnReference()
+                             {
+                                 ColumnName = IntegratedConstants.EndTS,
+                                 TableReference = selectedTable.TableAlias
+                             }
+                         },
+                     };
+                     OperatorRestriction endTSNull = new OperatorRestriction()
+                     {
+                         LHS = new ColumnOperand()
+                         {
+                             Column = new ColumnReference()
+                             {
+                                 ColumnName = IntegratedConstants.EndTS,
+                                 TableReference = selectedTable.TableAlias
+                             }
+                         },
+                         Op = RestrictionOperator.IS
+     ,
+                         RHS = new LiteralOperand()
+                         {
+                             Literal = "NULL"
+                         }
+                     };
+                     var OrRestriction = new OrRestriction();
+                     OrRestriction.Restrictions = new Restriction[] { endTsLt, endTSNull };
+                     var AndRestriction = new AndRestriction();
+                     AndRestriction.Restrictions = new Restriction[] { startTs, OrRestriction };
+                     TimeStampRestrictions.Add(AndRestriction);
+                 }
+                 //c.) add timestamp parts to it
+                 if (newOperation.Restriction != null)
+                     TimeStampRestrictions.Add(newOperation.Restriction);
 
-            //d.) render it and return
+                 newOperation.Restriction = new AndRestriction() { Restrictions = TimeStampRestrictions.ToArray() };
+
+                 //d.) render it and return
 
 
 
-            String originalSerialized = JsonSerializer.SerializeObject(s);
-            String originalrenderd = cRUDHandler.RenderSelectOperation(s);
+                 originalSerialized = JsonSerializer.SerializeObject(s);
+                 originalrenderd = cRUDHandler.RenderSelectOperation(s);
 
-            String select = cRUDHandler.RenderSelectOperation(newOperation);
-            String RewrittenSerialized = JsonSerializer.SerializeObject(newOperation);
+                 select = cRUDHandler.RenderSelectOperation(newOperation);
+                 RewrittenSerialized = JsonSerializer.SerializeObject(newOperation);
 
-            String selectHash = cRUDHandler.RenderHashSelect(newOperation);
-            String selectHashSerialized = cRUDHandler.RenderHashSelect(newOperation);
+                 selectHash = cRUDHandler.RenderHashSelect(newOperation);
+                 selectHashSerialized = JsonSerializer.SerializeObject(newOperation);
+
+                 return new String[] { selectHash, select };
+             };
 
             //f.) Execute it
-            DataTable normResult = null;
-            DataTable hashTable = null;
+
             String hash = null;
             Guid guid = Guid.NewGuid();
-            String additionalInfos = JsonSerializer.SerializeObject(table_to_ids);
-            String time = cRUDHandler.CRUDRenderer.SerializeDateTime(queryTime);
-            con.DoTransaction((trans, c) =>
-            {
-                normResult = con.ExecuteQuery(select, c);
-                hashTable = con.ExecuteQuery(selectHash, c);
-                hash = hashTable.Select().First().Field<String>(0);
-                String insert = qs.RenderInsert(originalrenderd, originalSerialized, RewrittenSerialized, select, time, hash, guid, selectHash, selectHashSerialized, additionalInfos);
-                long? id = con.ExecuteInsert(insert, c);
-                System.Diagnostics.Debug.WriteLine(id.Value);
-                trans.Commit();
-            });
+            String time = cRUDHandler.CRUDRenderer.SerializeDateTime(queryTime);   
+
+
+            Func<DataTable, DataTable, String> RenderInsert = (hashTable, SelectTable) =>
+              {
+                  normResult = SelectTable;
+                  hash = hashTable.Select().First().Field<String>(0);
+                  String additionalInfos = JsonSerializer.SerializeObject(table_to_ids);
+
+                  String insert = qs.RenderInsert(originalrenderd, originalSerialized, RewrittenSerialized, select, time, hash, guid, selectHash, selectHashSerialized, additionalInfos);
+                  return insert;
+              };
+
+            Action<String> log = (logst) => { System.Diagnostics.Debug.WriteLine(logst); };
+            IntegratedQSSelectExecuter executer = new IntegratedQSSelectExecuter();
+            long id = executer.ExecuteStatementsOnLockedTables(RenderInsert, RenderSelectStatements, tablesToLock,con,cRUDHandler.CRUDRenderer,schemaManager, MetaTables, metaManager, log);
+
 
             var execResult = new QueryStoreSelectResult()
             {
@@ -375,6 +404,7 @@ namespace QubaDC.Integrated
                 GUID = guid
 
             };
+
             return execResult;
         }
 
